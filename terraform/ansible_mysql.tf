0a04993b3c8126fd8bb947b236943efaef4863ce
Andrey Maltsev <malzew@gmail.com>
2022-09-22 07:28:21 +0500

    Task #4 complite

--- terraform/ansible_mysql.tf
+++ terraform/ansible_mysql.tf
@@ -8,23 +8,13 @@ resource "null_resource" "mysql_install" {
   ]
 }
 
-resource "null_resource" "mysql_create_db" {
-  provisioner "local-exec" {
-    command = "ANSIBLE_FORCE_COLOR=1 ansible-playbook -i ../ansible/inventory ../ansible/mysql_create_db.yml --extra-vars 'database_name=${var.database.database_name} database_user=${var.database.database_user} database_password=${var.database.database_password}'"
-  }
-
-  depends_on = [
-    null_resource.mysql_install
-  ]
-}
-
 resource "null_resource" "mysql_config_master" {
   provisioner "local-exec" {
     command = "ANSIBLE_FORCE_COLOR=1 ansible-playbook -i ../ansible/inventory ../ansible/mysql_config.yml --extra-vars 'mysql_hosts=mysql_master conf_file=/etc/mysql/mysql.conf.d/mysqld.cnf bind_address=0.0.0.0 server_id=1 binlog_do_db=${var.database.database_name}'"
   }
 
   depends_on = [
-    null_resource.mysql_create_db
+    null_resource.mysql_install
   ]
 }
 
@@ -34,7 +24,17 @@ resource "null_resource" "mysql_config_slave" {
   }
 
   depends_on = [
-    null_resource.mysql_create_db
+    null_resource.mysql_install
+  ]
+}
+
+resource "null_resource" "mysql_create_db" {
+  provisioner "local-exec" {
+    command = "ANSIBLE_FORCE_COLOR=1 ansible-playbook -i ../ansible/inventory ../ansible/mysql_create_db.yml --extra-vars 'mysql_hosts=mysql database_name=${var.database.database_name} database_user=${var.database.database_user} database_password=${var.database.database_password}'"
+  }
+
+  depends_on = [
+    null_resource.mysql_install
   ]
 }
 
@@ -44,16 +44,16 @@ resource "null_resource" "mysql_replication_master" {
   }
 
   depends_on = [
-    null_resource.mysql_config_master
+    null_resource.mysql_create_db
   ]
 }
 
 resource "null_resource" "mysql_replication_slave" {
   provisioner "local-exec" {
-    command = "ANSIBLE_FORCE_COLOR=1 ansible-playbook -i ../ansible/inventory ../ansible/mysql_replication.yml --extra-vars 'mysql_hosts=mysql_slave mysql_replication_role=slave mysql_replication_master=${yandex_compute_instance.db01.network_interface[0].ip_address} database_user=${var.database.database_user} database_password=${var.database.database_password} database_name=${var.database.database_name}'"
+    command = "ANSIBLE_FORCE_COLOR=1 ansible-playbook -i ../ansible/inventory ../ansible/mysql_replication.yml --extra-vars 'mysql_hosts=mysql_slave mysql_replication_role=slave mysql_replication_master=${yandex_compute_instance.db01.network_interface[0].ip_address} mysql_replication_master_ext=${yandex_compute_instance.db01.network_interface[0].nat_ip_address} database_user=${var.database.database_user} database_password=${var.database.database_password} database_name=${var.database.database_name}'"
   }
 
   depends_on = [
-    null_resource.mysql_config_master
+    null_resource.mysql_create_db
   ]
 }
