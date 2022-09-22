5d164a1a17ab66d8d12297d3877d9ef1131ab96d
Andrey Maltsev <malzew@gmail.com>
2022-09-23 00:27:55 +0500

    Task #6

--- terraform/ansible_runner.tf
+++ terraform/ansible_runner.tf
@@ -1,6 +1,6 @@
 resource "null_resource" "runner_install" {
   provisioner "local-exec" {
-    command = "ANSIBLE_FORCE_COLOR=1 ansible-playbook -i ../ansible/inventory ../ansible/gitlab_runner.yml"
+    command = "ANSIBLE_FORCE_COLOR=1 ansible-playbook -i ../ansible/inventory ../ansible/gitlab_runner.yml --extra-vars 'registration_token=GR1348941tTiyaiSVQpzpeAsLAj_2 domain_name=gitlab.${var.dns_zone}'"
   }
 
   depends_on = [
